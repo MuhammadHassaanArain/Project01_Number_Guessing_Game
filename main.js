053953d2a9c02375043d3fdb387ec023301ae88f
Hassaan Arain <rafatnaz21@gmail.com>
2024-03-27 14:12:54 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let number = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "guessedNumber",
+        type: "number",
+        message: "Guess a number between 1 to 5",
+    }
+]);
+if (answer.guessedNumber === number) {
+    console.log("Congratulations you just guessed the correct one !");
+}
+else {
+    console.log("incorret number Try Again");
+}
+;
+if (answer.guessedNumber != number) {
+    console.log(`Correct answer was ${number}`);
+}
+;
